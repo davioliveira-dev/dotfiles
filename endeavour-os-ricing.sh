f6f19ab156be259c2839c9e7698ff26dabe54690
Davi Oliveira <davioliveira.java@gmail.com>
2024-09-10 09:03:20 -0300

    Create endeavour-os-ricing.sh

---
+++ endeavour-os-ricing.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+echo "Installing deps with Yay"
+
+yay -S gnome-software ttf-jetbrains-mono-nerd morewaita git ripgrep curl wget unzip base-devel wl-clipboard github-cli zsh jdk8-openjdk bat gnome-tweaks apple_cursor flatpak noisetorch adw-gtk-theme wireguard-tools visual-studio-code-bin jetbrains-toolbox
+
+echo "Downloading fonts"
+
+git clone https://github.com/sahibjotsaggu/San-Francisco-Pro-Fonts.git
+unzip San-Francisco-Pro-Fonts-master.zip
+cd San-Francisco-Pro-Fonts-master 
+rm -rf README.md 
+sudo mv * /usr/share/fonts
+
+# Set fonts
+gsettings set org.gnome.desktop.interface font-name 'SF Pro Rounded 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMono Nerd Font 12'
+
+# Set rendering and antialiasing
+gsettings set org.gnome.settings-daemon.plugins.xsettings hinting 'medium'
+gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing 'grayscale'
+
+# Enable minimize button and center new windows
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+gsettings set org.gnome.mutter center-new-windows true
+
+# Disable mouse acceleration
+gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
+
+# Add Portuguese keyboard layout
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'br')]"
+
+# Set window switch shortcut to Alt-Tab
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Alt>Tab']"
+
+# Download and set profile picture
+wget -O ~/profile.jpg https://avatars.githubusercontent.com/u/53924113?v=4
+sudo cp ~/profile.jpg /var/lib/AccountsService/icons/$USER
+echo "[User]" | sudo tee /var/lib/AccountsService/users/$USER
+echo "Icon=/var/lib/AccountsService/icons/$USER" | sudo tee -a /var/lib/AccountsService/users/$USER
+
+# Cleanup
+rm ~/profile.jpg
+
+echo "GNOME settings have been applied successfully."
+
+# Install applications
+echo "Installing Flatpak applications..."
+
+declare -A apps=(
+    ["chrome"]="com.google.Chrome"
+    ["discord"]="com.discordapp.Discord"
+    ["cohesion"]="io.github.brunofin.Cohesion"
+    ["blackbox"]="com.raggesilver.BlackBox"
+    ["cartero"]="es.danirod.Cartero"
+    ["slack"]="com.slack.Slack"
+    ["alpaca"]="com.jeffser.Alpaca"
+    ["extension-manager"]="com.mattjakeman.ExtensionManager"
+)
+
+for app in "${!apps[@]}"; do
+    echo "Installing $app..."
+    flatpak install -y flathub "${apps[$app]}"
+done
+
+echo "All Flatpak applications installed."
